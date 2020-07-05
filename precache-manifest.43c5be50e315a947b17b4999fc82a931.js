ae3e9b9287b561f6e50a9cfa981a1ec94ee01b03
Kinshuk Shah <kinshukshah@gmail.com>
2020-07-05 21:01:53 +0530

    Updates

---
+++ precache-manifest.43c5be50e315a947b17b4999fc82a931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c258bb61fcbe1e19dc0456e8c78569d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3e0c0770692c8b053fbe",
+    "url": "/monsters-rolodex/static/css/main.3a07cdaa.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0c0770692c8b053fbe",
+    "url": "/monsters-rolodex/static/js/main.edb6ed09.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
