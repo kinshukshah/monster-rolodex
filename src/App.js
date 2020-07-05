c5f47d2d102c12687f5e2d9284b20f39b61e37c8
Kinshuk Shah <kinshukshah@gmail.com>
2020-07-05 21:09:04 +0530

    Adding files for GitHub

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component.jsx'
+import { SearchBox } from './components/search-box/search-box.component.jsx'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters:[],
+      searchField:''
+    }
+  }
+  handleChange=(e) =>{
+    this.setState({searchField:e.target.value});
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((user) => { this.setState({ monsters:user }) })
+  }
+  render(){
+    const { monsters,searchField }=this.state;
+    let filteredList=monsters.filter((monster) =>{
+      return monster.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    return (
+      <div className="App">
+      <h1>Monster Rolodex</h1>
+      <SearchBox placeholder='Search Monster...' handleChange={ this.handleChange } ></SearchBox>
+      <CardList monsters={ filteredList }></CardList>
+   
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../search-box/search-box.styles.css'
+
+export const SearchBox=({ placeholder,handleChange }) =>(
+    <input type='search'
+       placeholder={ placeholder } 
+       onChange={ handleChange }></input>
+);
